7249a33da7b9db274275cbfd73856772912f1ffb
Kody Horiuchi <kody.a.horiuchi@gmail.com>
2022-11-28 11:53:53 -0500

    added functionality

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey as survey
 
 app = Flask(__name__)
-app.config['SSECRET_KEY'] = "oh so secret"
+app.config['SSECRET_KEY'] = "oh-so-secret"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
@@ -13,4 +13,4 @@ debug = DebugToolbarExtension(app)
 def index():
     """Return homepage."""
 
-    return render_template("start.html")
+    return render_template("start.html", survey=survey)
