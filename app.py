8dadb3dfd9a8e2e813b0b1dcbcb627a2c7d2b474
Kody Horiuchi <kody.a.horiuchi@gmail.com>
2022-11-28 12:10:45 -0500

    added question

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey as survey
 
+RESPONSES_KEY = "responses"
+
 app = Flask(__name__)
 app.config['SSECRET_KEY'] = "oh-so-secret"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
@@ -14,3 +16,19 @@ def index():
     """Return homepage."""
 
     return render_template("start.html", survey=survey)
+
+
+@app.route('/begin', methods=['POST'])
+def start_survey():
+    """Clears session and redirect to question"""
+
+    session[RESPONSES_KEY] = []
+    return redirect("/questions/0")
+
+
+@app.route("/questions/<int:qid>")
+def show_question(qid):
+    """Shows the question"""
+
+    question = survey.questions[qid]
+    return render_template("question.html", question_num=qid, question=question)
