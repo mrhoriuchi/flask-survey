3864f06d2fc64b148628c63d140d7d96c34c9df8
Kody Horiuchi <kody.a.horiuchi@gmail.com>
2022-11-28 11:18:53 -0500

    added templates

---
+++ app.py
@@ -0,0 +1,8 @@
+from flask import Flask, request, render_template
+from flask_debugtoolbar import DebugToolBarExtension
+
+app = Flask(__name__)
+app.config['SSECRET_KEY'] = "oh so secret"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+debug = DebugToolBarExtension(app)
